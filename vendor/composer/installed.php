59a6d6e64e356e045a366ac05725d1e13e7e66f8
Dickson Agyei <dicksonjunior42@gmail.com>
2023-05-03 16:58:50 +0000

    updated version number in composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'alle-ai/anthropic-api-php',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
+        'pretty_version' => '1.0.2',
+        'version' => '1.0.2.0',
         'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'alle-ai/anthropic-api-php' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
             'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
