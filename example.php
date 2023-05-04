a6c1d51a6837054bed21a5421105b65dc24c07cf
Dickson Agyei <dicksonjunior42@gmail.com>
2023-05-04 02:14:28 +0000

    Update example.php
    Updated the prompt

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ $api_key = 'your-anthropic-api-key';
 
 $anthropic_api = new Alle_AI\Anthropic\AnthropicAPI($api_key);
 $data = array(
-    'prompt' => 'The quick brown fox jumps over the lazy dog',
+    'prompt' => 'Write a short poem about Artificial Intelligence',
     'model' => 'claude-v1',
     'max_tokens_to_sample' => 300,
     'stop_sequences' => array("\n\nHuman:")
