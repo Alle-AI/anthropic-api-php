f917409ca08fa817f6faee78d6652375b5695124
Dickson Agyei <64383297+dicksonagyei@users.noreply.github.com>
2023-10-08 18:15:28 -0700

    Update example.php
    updated the model to 'claude-2'

--- example.php
+++ example.php
@@ -7,7 +7,7 @@ $anthropic_api = new Alle_AI\Anthropic\AnthropicAPI($api_key);
 $prompt = "Write a short poem about AI.";
 $data = array(
     'prompt' => '\n\nHuman: '.$prompt.'\n\nAssistant:', // Be sure to append these appropriately.
-    'model' => 'claude-v1',
+    'model' => 'claude-2',
     'max_tokens_to_sample' => 300,
     'stop_sequences' => array("\n\nHuman:")
   );
